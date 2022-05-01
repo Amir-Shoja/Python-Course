475bc4768a225da3cd4207065aca831022e64936
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-05-01 23:31:10 +0430

    Update 2. Non_duplicate_random.py

--- Assignment_3/2. Non_duplicate_random.py
+++ Assignment_3/2. Non_duplicate_random.py
@@ -2,10 +2,8 @@ import random
 
 array = []
 n = int(input("Enter size array : "))
-
-for i in range(n):
+while len(array) <= n:
     temp = random.randint(1, 100)
     if temp not in array:
         array.append(temp)
-
 print("array : ", array)
