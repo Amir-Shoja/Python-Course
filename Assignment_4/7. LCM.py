21bbf926d9175926f98f1858baf1e8df220862b2
Amir Shoja <83409092+Amir-Shoja@users.noreply.github.com>
2022-05-02 02:54:55 +0430

    Add files via upload

---
+++ Assignment_4/7. LCM.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
+a = int(input("Enter num1 : "))
+b = int(input("Enter num2 : "))
+
+print("lcm = ", (a * b) / gcd(a, b))
